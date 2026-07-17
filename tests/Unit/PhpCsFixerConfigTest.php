e785b63cad0cafb4f86ea9c88b5bcbad95df2b45
Benjamin Fahl <git@fahl-design.de>
2026-07-17 22:26:50 +0200

    test(config): add tests to see expected rules

--- tests/Unit/PhpCsFixerConfigTest.php
+++ tests/Unit/PhpCsFixerConfigTest.php
@@ -9,15 +9,74 @@ use WebProject\PhpCsFixerConfig\Tests\Support\UnitTester;
 class PhpCsFixerConfigTest extends \Codeception\Test\Unit
 {
     protected UnitTester $tester;
+    protected \PhpCsFixer\Config $phpCsFixerConfig;
+
+    protected function _setUp(): void
+    {
+        $this->phpCsFixerConfig = new PhpCsFixerConfig()(__DIR__);
+
+        parent::_setUp();
+    }
 
     // tests
     public function testNameIsSet(): void
     {
         // Arrange
-        $config = new PhpCsFixerConfig()(__DIR__);
         // Act
-        $name = $config->getName();
+        $name = $this->phpCsFixerConfig->getName();
         // Assert
         self::assertSame(PhpCsFixerConfig::NAME, $name);
+    }
+
+    public function testRulesAreAsExpected(): void
+    {
+        // Arrange
+        $expectedRules =  [
+            '@PSR12'                 => true,
+            '@Symfony'               => true,
+            '@Symfony:risky'         => true,
+            '@PhpCsFixer:risky'      => true,
+            '@PHP8x3Migration'       => true,
+            '@DoctrineAnnotation'    => true,
+            'binary_operator_spaces' => [
+                'default'   => 'align',
+                'operators' => [
+                    '??' => 'single_space',
+                ],
+            ],
+            'concat_space' => [
+                'spacing' => 'one',
+            ],
+            'encoding'                     => true,
+            'blank_lines_before_namespace' => true,
+            'blank_line_after_opening_tag' => false,
+            'strict_param'                 => true,
+            'no_useless_else'              => true,
+            'no_useless_return'            => true,
+            'modernize_types_casting'      => true,
+            'declare_strict_types'         => true,
+            'dir_constant'                 => true,
+            'php_unit_dedicate_assert'     => [
+                'target' => 'newest',
+            ],
+            'combine_nested_dirname' => true,
+            'ordered_imports'        => [
+                'sort_algorithm' => 'alpha',
+            ],
+            'global_namespace_import' => [
+                'import_classes'   => true,
+                'import_functions' => true,
+                'import_constants' => true,
+            ],
+            'phpdoc_to_comment'                                => false,
+            'nullable_type_declaration_for_default_null_value' => true,
+            'blank_line_between_import_groups'                 => false,
+        ];
+
+        // Act
+        $rulesFromConfig = $this->phpCsFixerConfig->getRules();
+
+        // Assert
+        self::assertSame($expectedRules, $rulesFromConfig);
     }
 }
